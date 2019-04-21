80fd0ded4be0430ac4aded46802896cb7173c16a
AndreGGPereira <andreggp@gmail.com>
2019-04-21 20:35:29 -0300

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
    // baseURL: 'https://dependencia.herokuapp.com/'
-   baseURL: 'https://ancient-headland-55662.herokuapp.com/'
+   baseURL: 'https://stark-hamlet-45050.herokuapp.com/'
   //baseURL: 'https://192.168.0.6:8080'
 });
 
