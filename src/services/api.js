4dc9a9d55a7cd9e9233dbe56ebd0d05b8bc84943
AndreGGPereira <andreggp@gmail.com>
2019-04-21 16:51:52 -0300

    adasdasd

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
    // baseURL: 'https://dependencia.herokuapp.com/'
-  // baseURL: 'https://ancient-headland-55662.herokuapp.com/'
-  baseURL: 'https://192.168.0.6:8080'
+   baseURL: 'https://ancient-headland-55662.herokuapp.com/'
+  //baseURL: 'https://192.168.0.6:8080'
 });
 
 export default api;
