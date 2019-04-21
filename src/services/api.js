3594f9368c1f75e3cd8a4726f601e8af0dbe7002
AndreGGPereira <andreggp@gmail.com>
2019-04-21 14:31:10 -0300

    teste cors header

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
    // baseURL: 'https://dependencia.herokuapp.com/'
   // baseURL: 'https://ancient-headland-55662.herokuapp.com/'
-  baseURL: 'http://192.168.0.6:8080'
+  baseURL: 'https://192.168.0.6:8080'
 });
 
 export default api;
