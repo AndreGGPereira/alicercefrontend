61fedd2d05b7e5970dc8b801029236f409df3dd7
AndreGGPereira <41800272+AndreGGPereira@users.noreply.github.com>
2019-04-30 13:40:43 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
    // baseURL: 'https://dependencia.herokuapp.com/'
-   baseURL: 'https://stark-hamlet-45050.herokuapp.com/'
+   baseURL: 'https://glacial-castle-37039.herokuapp.com/'
   //baseURL: 'https://192.168.0.6:8080'
 });
 
